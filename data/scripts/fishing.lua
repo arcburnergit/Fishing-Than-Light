8d12e85fa440d6e0ec6a68bca1b5906a8866e629
arc <arc.onlineemail@gmail.com>
2024-04-27 02:13:05 +1000

    basic fishing stuff

---
+++ data/scripts/fishing.lua
@@ -0,0 +1,148 @@
+mods.fishing = {}
+
+-----------------------
+-- UTILITY FUNCTIONS --
+-----------------------
+
+
+-- Get a table for a userdata value by name
+local function userdata_table(userdata, tableName)
+    if not userdata.table[tableName] then userdata.table[tableName] = {} end
+    return userdata.table[tableName]
+end
+
+local function get_random_point_in_radius(center, radius)
+    r = radius * math.sqrt(math.random())
+    theta = math.random() * 2 * math.pi
+    return Hyperspace.Pointf(center.x + r * math.cos(theta), center.y + r * math.sin(theta))
+end
+
+local function get_point_local_offset(original, target, offsetForwards, offsetRight)
+    local alpha = math.atan((original.y-target.y), (original.x-target.x))
+    --print(alpha)
+    local newX = original.x - (offsetForwards * math.cos(alpha)) - (offsetRight * math.cos(alpha+math.rad(90)))
+    --print(newX)
+    local newY = original.y - (offsetForwards * math.sin(alpha)) - (offsetRight * math.sin(alpha+math.rad(90)))
+    --print(newY)
+    return Hyperspace.Pointf(newX, newY)
+end
+
+local function vter(cvec)
+    local i = -1
+    local n = cvec:size()
+    return function()
+        i = i + 1
+        if i < n then return cvec[i] end
+    end
+end
+
+-- Find ID of a room at the given location
+local function get_room_at_location(shipManager, location, includeWalls)
+    return Hyperspace.ShipGraph.GetShipInfo(shipManager.iShipId):GetSelectedRoom(location.x, location.y, includeWalls)
+end
+
+-- Returns a table of all crew belonging to the given ship on the room tile at the given point
+local function get_ship_crew_point(shipManager, x, y, maxCount)
+    res = {}
+    x = x//35
+    y = y//35
+    for crewmem in vter(shipManager.vCrewList) do
+        if crewmem.iShipId == shipManager.iShipId and x == crewmem.x//35 and y == crewmem.y//35 then
+            table.insert(res, crewmem)
+            if maxCount and #res >= maxCount then
+                return res
+            end
+        end
+    end
+    return res
+end
+
+local function get_ship_crew_room(shipManager, roomId)
+    local radCrewList = {}
+    for crewmem in vter(shipManager.vCrewList) do
+        if crewmem.iShipId == shipManager.iShipId and crewmem.iRoomId == roomId then
+            table.insert(radCrewList, crewmem)
+        end
+    end
+    return radCrewList
+end
+
+-- written by kokoro
+local function convertMousePositionToEnemyShipPosition(mousePosition)
+    local cApp = Hyperspace.Global.GetInstance():GetCApp()
+    local combatControl = cApp.gui.combatControl
+    local position = 0--combatControl.position -- not exposed yet
+    local targetPosition = combatControl.targetPosition
+    local enemyShipOriginX = position.x + targetPosition.x
+    local enemyShipOriginY = position.y + targetPosition.y
+    return Hyperspace.Point(mousePosition.x - enemyShipOriginX, mousePosition.y - enemyShipOriginY)
+end
+
+-- Returns a table where the indices are the IDs of all rooms adjacent to the given room
+-- and the values are the rooms' coordinates
+local function get_adjacent_rooms(shipId, roomId, diagonals)
+    local shipGraph = Hyperspace.ShipGraph.GetShipInfo(shipId)
+    local roomShape = shipGraph:GetRoomShape(roomId)
+    local adjacentRooms = {}
+    local currentRoom = nil
+    local function check_for_room(x, y)
+        currentRoom = shipGraph:GetSelectedRoom(x, y, false)
+        if currentRoom > -1 and not adjacentRooms[currentRoom] then
+            adjacentRooms[currentRoom] = Hyperspace.Pointf(x, y)
+        end
+    end
+    for offset = 0, roomShape.w - 35, 35 do
+        check_for_room(roomShape.x + offset + 17, roomShape.y - 17)
+        check_for_room(roomShape.x + offset + 17, roomShape.y + roomShape.h + 17)
+    end
+    for offset = 0, roomShape.h - 35, 35 do
+        check_for_room(roomShape.x - 17,               roomShape.y + offset + 17)
+        check_for_room(roomShape.x + roomShape.w + 17, roomShape.y + offset + 17)
+    end
+    if diagonals then
+        check_for_room(roomShape.x - 17,               roomShape.y - 17)
+        check_for_room(roomShape.x + roomShape.w + 17, roomShape.y - 17)
+        check_for_room(roomShape.x + roomShape.w + 17, roomShape.y + roomShape.h + 17)
+        check_for_room(roomShape.x - 17,               roomShape.y + roomShape.h + 17)
+    end
+    return adjacentRooms
+end
+
+mods.fishing.rods = {}
+local rods = mods.fishing.rods
+rods["FISHING_ROD_1"] = 5
+rods["FISHING_ROD_2"] = 10
+rods["FISHING_ROD_3"] = 20
+
+script.on_internal_event(Defines.InternalEvents.PROJECTILE_INITIALIZE, function(projectile, weaponBlueprint)
+    local fishingData = rods[weapon.blueprint.name]
+    if fishingData then
+        Hyperspace.playerVariables.fish_this_jump = 1
+        Hyperspace.playerVariables.fish_active = 1
+    end
+end)
+
+script.on_internal_event(Defines.InternalEvents.SHIP_LOOP, function(shipManager)
+    for weapon in vter(shipManager:GetWeaponList()) do
+        local fishingData = rods[weapon.blueprint.name]
+        if fishingData then
+            if Hyperspace.playerVariables.fish_this_sector == 1 then
+                weapon.boostLevel == 2
+            elseif Hyperspace.playerVariables.fish_active == 1 then
+                weapon.cooldown.first = 0.1
+            elseif Hyperspace.playerVariables.fish_this_jump == 1 then
+                weapon.boostLevel == 2
+            end
+        end
+    end
+
+    if Hyperspace.playerVariables.fish_active == 1 then
+    end
+end)
+
+script.on_internal_event(Defines.InternalEvents.JUMP_ARRIVE, function(shipManager)
+    if Hyperspace.playerVariables.fish_this_jump == 1 then
+        Hyperspace.playerVariables.fish_this_jump = 0
+        Hyperspace.playerVariables.fish_this_sector = 1
+    end
+end)
